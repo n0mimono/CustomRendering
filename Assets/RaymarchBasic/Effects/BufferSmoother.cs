3a730d1201d93d50adde794ec61650d7e3f54822
luke2 <r15yokote@gmail.com>
2016-09-11 20:11:04 +0900

    little tuning

---
+++ Assets/RaymarchBasic/Effects/BufferSmoother.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+public class BufferSmoother : MonoBehaviour {
+  public Material mat;
+  public BuiltinRenderTextureType type;
+
+  private Dictionary<Camera, CommandBuffer> buffers = new Dictionary<Camera, CommandBuffer>();
+  private static readonly CameraEvent TargetCameraEvent = CameraEvent.AfterGBuffer;
+
+  private void CleanUp() {
+    foreach (var buf in buffers.Where(b => b.Key != null)) {
+      buf.Key.RemoveCommandBuffer (TargetCameraEvent, buf.Value);
+    }
+    buffers.Clear ();
+  }
+
+  void OnEnable() {
+    CleanUp ();
+  }
+
+  void OnDisable() {
+    CleanUp ();
+  }
+
+  void Update() {
+    if (!gameObject.activeInHierarchy || !enabled) {
+      CleanUp ();
+      return;
+    }
+
+    Camera current = Camera.current;
+    if (current == null) return;
+
+    if (mat == null) return;
+
+    if (buffers.ContainsKey(current)) return;
+
+    CommandBuffer buf = null;
+    if (buffers.ContainsKey(current)) {
+      buf = buffers[current];
+      buf.Clear();
+    } else {
+      buf = new CommandBuffer();
+      buf.name = GetType ().Name;
+      buffers[current] = buf;
+      current.AddCommandBuffer(TargetCameraEvent, buf);
+    }
+    Reconstruct (buf);
+  }
+
+  public void Reconstruct(CommandBuffer buf) {
+    int id = Shader.PropertyToID("_Buffer");
+    buf.GetTemporaryRT(id, -1, -1);
+    buf.Blit(type, id);
+
+    buf.Blit(id, type, mat);
+
+    buf.ReleaseTemporaryRT (id);
+  }
+
+}
---
+++ Assets/RaymarchBasic/Effects/BufferSmoother.shader
@@ -0,0 +1,45 @@
+﻿Shader "Hidden/BufferSmoother" {
+	Properties {
+		_MainTex ("Texture", 2D) = "white" {}
+    _SampleScale ("Sample Scale", Float) = 0
+    _CenterAmp ("Center Amplitude", Range(0,1)) = 0.5
+	}
+	SubShader {
+		Cull Off ZWrite Off ZTest Always
+
+		Pass {
+			CGPROGRAM
+			#pragma vertex vert_img
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+			
+			sampler2D _MainTex;
+      float4 _MainTex_TexelSize;
+
+      float _SampleScale;
+      float _CenterAmp;
+
+			fixed4 frag (v2f_img i) : SV_Target {
+        float2 uv = i.uv;
+        float2 d  = _MainTex_TexelSize.xy * _SampleScale;
+
+        float c = _CenterAmp;
+        float r = (1 - c) / 8;
+
+				float4 col =
+            tex2D(_MainTex, uv) * c +
+            tex2D(_MainTex, uv + float2( d.x,   0)) * r +
+            tex2D(_MainTex, uv + float2( d.x, d.y)) * r +
+            tex2D(_MainTex, uv + float2(   0, d.y)) * r +
+            tex2D(_MainTex, uv + float2(-d.x, d.y)) * r +
+            tex2D(_MainTex, uv + float2(-d.x,   0)) * r +
+            tex2D(_MainTex, uv + float2(-d.x,-d.y)) * r +
+            tex2D(_MainTex, uv + float2(   0,-d.y)) * r +
+            tex2D(_MainTex, uv + float2( d.x,-d.y)) * r ;
+
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
