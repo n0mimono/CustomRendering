cdfe7c8a5ac2b3f9ee6101d8682b5f319bb34579
Ryota Yokote <yokote.ryota@donuts.ne.jp>
2016-09-12 11:55:35 +0900

    add frame recorder

---
+++ Assets/FrameRecorder/FrameRecorder.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+public class FrameRecorder : MonoBehaviour {
+
+  private const int    FrameRate = 30;
+  private const string SaveDir = "ScreenShots";
+  private const string Prefix  = "frame_";
+
+  private int   width  = 640;
+  private int   height = 480;
+  private float wait   = 1f;
+
+  private RenderTexture rt;
+  private Texture2D     tex;
+  private Camera        cam;
+
+  IEnumerator Start() {
+    Time.captureFramerate = FrameRate;
+    System.IO.Directory.CreateDirectory(SaveDir);
+    yield return null;
+
+    cam  = GetComponent<Camera> ();
+    rt   = new RenderTexture (width, height, 24);
+    tex  = new Texture2D (width, height, TextureFormat.RGB24, false);
+    yield return new WaitForSeconds (wait);
+
+    Debug.Log ("Start Record");
+    while (true) {
+      yield return new WaitForEndOfFrame ();
+
+      Capture ();
+    }
+  }
+
+  private void Capture() {
+    RenderTexture target = cam.targetTexture;
+
+    cam.targetTexture = rt;
+    cam.Render ();
+    RenderTexture.active = rt;
+    tex.ReadPixels (new Rect (0, 0, width, height), 0, 0);
+    tex.Apply ();
+
+    cam.targetTexture = target;
+    RenderTexture.active = null;
+
+    byte[] bytes = tex.EncodeToPNG();
+
+    string fileName = string.Format("{0}{1:D04}.png", Prefix, Time.frameCount);
+    var path = System.IO.Path.Combine(SaveDir, fileName);
+
+    System.IO.File.WriteAllBytes(path, bytes);
+  }
+
+}
---
+++ Assets/FrameRecorder/Rotater.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotater : MonoBehaviour {
+  public Vector3 speed;
+
+  void Update() {
+    transform.Rotate (speed * Time.deltaTime);
+  }
+
+}
