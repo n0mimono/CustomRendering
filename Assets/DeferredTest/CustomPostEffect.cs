1b6ee6f01d9b89bee069f444b1d73cd62f2280b2
luke2 <r15yokote@gmail.com>
2016-09-17 18:22:48 +0900

    add rim

---
+++ Assets/DeferredTest/CustomPostEffect.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class CustomPostEffect : MonoBehaviour {
+  public Material mat;
+  public bool     useIvp;
+
+  void OnEnable() {
+    GetComponent<Camera> ().depthTextureMode = DepthTextureMode.DepthNormals;
+  }
+
+  void OnRenderImage(RenderTexture src, RenderTexture dst) {
+    if (mat != null) {
+      if (useIvp) {
+        Camera    camera = GetComponent<Camera> ();
+        Matrix4x4 view   = camera.worldToCameraMatrix;
+        Matrix4x4 proj   = GL.GetGPUProjectionMatrix (camera.projectionMatrix, false);
+        Matrix4x4 vp     = proj * view;
+        Matrix4x4 ivp    = vp.inverse;
+
+        mat.SetMatrix ("_InvViewProj", ivp);
+      }
+
+      Graphics.Blit (src, dst, mat);
+    } else {
+      Graphics.Blit (src, dst);
+    }
+  }
+
+}
